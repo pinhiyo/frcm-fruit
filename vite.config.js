4ee0c82e0be6f3445a1fc18a9769861e58864c75
pinhiyo <126062967+pinhiyo@users.noreply.github.com>
2026-03-28 01:23:44 +0900

    Update base path in vite.config.js for deployment
    Changed base path for assets to support deployment.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: './', // Use relative paths for assets so GitHub pages works
+    base: '/frcm-fruit/', // ここを './' から '/frcm-fruit/' に変更しました
 });
